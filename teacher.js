3741b42cabb9bb1cb33d497d34b68657d74cf7eb
Mfdal <maadimfdal@gmail.com>
2025-03-08 12:29:19 +0100

    last teacher commit

--- teacher.js
+++ teacher.js
@@ -80,33 +80,4 @@ router.get("/enrolledStudents/:course_id", async (req, res) => {
   }
 });
 
-// router.get("/enrolledStudents/:course_id", async (req, res) => {
-//   const { course_id } = req.params;
-//   const token = req.headers["authorization"];
-
-//   try {
-//     const students = await axios.get("http://localhost:3007/student/all", {
-//       headers: {
-//         Authorization: token,
-//       },
-//     });
-
-//     console.log("Response from student service:", students.data);
-
-//     if (!Array.isArray(students.data)) {
-//       return res
-//         .status(500)
-//         .json({ message: "Invalid data format from student service" });
-//     }
-
-//     const enrolledStudents = students.data.filter((student) =>
-//       student.cours.includes(course_id)
-//     );
-
-//     res.json(enrolledStudents);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
 module.exports = router;
